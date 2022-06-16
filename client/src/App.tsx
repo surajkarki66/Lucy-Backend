e4cf90df722260c6e5fc9acdc3b83458f4a5fee6
= <suraj.karki500@protonmail.com>
2022-06-16 21:56:35 +0545

    Added routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
-import Nav from "./components/Navs/Nav";
-import Chat from "./components/Chat/Chat";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
+import Nav from "./components/Navs/Nav";
+import Home from "./pages/Home/Home";
+import ChatPage from "./pages/ChatPage";
+
+const App: React.FC = () => {
+  const routes = (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/chat" element={<ChatPage />} />
+    </Routes>
+  );
   return (
     <div>
       <Nav />
-      <Chat />
+      {routes}
     </div>
   );
-}
+};
 
 export default App;
