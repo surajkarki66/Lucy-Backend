77bba40523d9f5f8f9656441b971d66e0926c5f0
surajkarki66 <suraj.karki500@protonmail.com>
2022-09-15 22:02:03 +0545

    Modified router

--- app/main/routers/chat_bot.py
+++ app/main/routers/chat_bot.py
@@ -1,8 +1,11 @@
 import random
 
-from fastapi import status
+from fastapi import status, Depends
+from sqlalchemy.orm import Session
 from fastapi.routing import APIRouter
 
+from app.main.models.models import Intent
+from app.main.infrastructure.database.db import get_db
 from app.main.services.intent_classification_service import IntentClassificationService
 from app.main.schemas.schemas import GetBotResponseSchema
 
@@ -11,7 +14,9 @@ router = APIRouter(prefix='/bot')
 
 
 @router.get("/chat", status_code=status.HTTP_200_OK, response_model=GetBotResponseSchema)
-def get_bot_response(message: str) -> dict : 
+def get_bot_response(message: str, db: Session = Depends(get_db)) -> dict : 
     inp_x=message.lower()
-    response, links = classification_service.get_response(inp_x)
+    pred = classification_service.predict(inp_x)
+    intent = db.query(Intent).filter(Intent.intent_no == int(pred)).first()
+    response, links = classification_service.get_response(intent.title)
     return {"message": response, "links": links}
