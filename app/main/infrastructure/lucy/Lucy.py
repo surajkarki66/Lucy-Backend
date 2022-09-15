5d79efae4283be2a0591b1a2254a65559a9d7033
surajkarki66 <suraj.karki500@protonmail.com>
2022-09-15 17:56:08 +0545

    Store no of output layer in a variable

--- app/main/infrastructure/lucy/Lucy.py
+++ app/main/infrastructure/lucy/Lucy.py
@@ -1,9 +1,10 @@
 import torch
 import torch.nn as nn
 
-class LucyDistilBERT(nn.Module):
+class Lucy(nn.Module):
   def __init__(self, pretrained_model):      
-       super(LucyDistilBERT, self).__init__()       
+       super(Lucy, self).__init__()    
+       self.len_labels = 38   
        self.pretrained_model = pretrained_model 
       
        # dropout layer
@@ -15,7 +16,7 @@ class LucyDistilBERT(nn.Module):
        # dense layer       
        self.fc1 = nn.Linear(768,512)       
        self.fc2 = nn.Linear(512,256)       
-       self.fc3 = nn.Linear(256,38)    
+       self.fc3 = nn.Linear(256,self.len_labels)    
 
        #softmax activation function
        self.softmax = nn.LogSoftmax(dim=1)      
@@ -39,6 +40,3 @@ class LucyDistilBERT(nn.Module):
       # apply softmax activation
       x = self.softmax(x)     
       return x
-
-
-
