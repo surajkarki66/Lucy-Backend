a7e0df06da254ebe43933bac573041d0886356ba
surajkarki66 <suraj.karki500@protonmail.com>
2022-09-15 19:57:50 +0545

    added intent router

--- app/initializer.py
+++ app/initializer.py
@@ -4,6 +4,7 @@ class IncludeAPIRouter(object):
         from app.main.routers.hello_world import router as router_hello_world
         from app.main.routers.chat_bot import router as router_chat_bot
         from app.main.routers.feedback import router as router_feedback
+        from app.main.routers.intent import router as router_intent
         from app.main.routers.auth import router as router_auth
         
         router = APIRouter()
@@ -11,6 +12,7 @@ class IncludeAPIRouter(object):
         router.include_router(router_chat_bot, prefix='/api/v2', tags=['chat_bot'])
         router.include_router(router_feedback, prefix='/api/v2', tags=['feedback'])
         router.include_router(router_auth, prefix="/api/v2", tags=["auth"])
+        router.include_router(router_intent, prefix="/api/v2", tags=["intent"])
 
         return router
 
