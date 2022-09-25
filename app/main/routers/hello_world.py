74f90be7631971967c3f5adff0dbabea066f57a2
surajkarki66 <suraj.karki500@protonmail.com>
2022-09-25 23:29:03 +0545

    Deleted

--- app/main/routers/hello_world.py
+++
@@ -1,9 +0,0 @@
-from fastapi.responses import JSONResponse
-from fastapi import APIRouter
-
-
-router = APIRouter()
-
-@router.get("/")
-def hello_world():
-    return JSONResponse(content={"message": "Hello World! 👍🏻"}, status_code=200)
