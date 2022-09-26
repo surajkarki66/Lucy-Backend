ac7ff08b177b1b8412231cff79a5714494b0d8c2
surajkarki66 <suraj.karki500@protonmail.com>
2022-09-26 15:59:46 +0545

    Modified sqlalchemy database url

--- app/infrastructure/database/db.py
+++ app/infrastructure/database/db.py
@@ -1,14 +1,12 @@
 from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from app.configs.config import settings
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.DATABASE_USERNAME}:\
-                            {settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:\
-                            {settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
-
+SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{settings.DATABASE_USERNAME}:{settings.DATABASE_PASSWORD}@{settings.DATABASE_HOSTNAME}:{settings.DATABASE_PORT}/{settings.DATABASE_NAME}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
