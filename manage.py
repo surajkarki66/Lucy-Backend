9901e584e90b234816a366c198788d04cb3b5cac
surajkarki66 <suraj.karki500@protonmail.com>
2022-09-26 16:00:43 +0545

    Removed some section of code

--- manage.py
+++ manage.py
@@ -3,4 +3,4 @@ import uvicorn
 from app.configs.config import settings
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host=settings.HOST, port=settings.PORT, use_colors=True,reload=True)
+    uvicorn.run("app.main:app", host=settings.HOST, port=settings.PORT, use_colors=True,reload=True)
