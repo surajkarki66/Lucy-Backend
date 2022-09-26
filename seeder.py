d8d626c9eaa8ccf3c3325ee69dc5ffa89741586e
surajkarki66 <suraj.karki500@protonmail.com>
2022-09-26 15:54:54 +0545

    Added seeder.py

---
+++ seeder.py
@@ -0,0 +1,21 @@
+from app.infrastructure.database.db import *
+
+def get_intent_label_query():
+    with open("./sql/intent_labels.sql") as file:
+        query = text(file.read())
+    file.close()
+    return query
+
+def get_response_query():
+    with open("./sql/responses.sql") as file:
+        query = text(file.read())
+    file.close()
+    return query
+
+
+if __name__ == "__main__":
+    with engine.connect() as con:
+        intent_label_query = get_intent_label_query()
+        response_query = get_response_query()
+        con.execute(intent_label_query)
+        con.execute(response_query)
